987421b9c1b74f6ebfbfa29a9946c5b143d84a12
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-07-17 13:38:58 +0900

    feat: GET  method of all tasks API

--- app/controllers/tasks.go
+++ app/controllers/tasks.go
@@ -32,10 +32,46 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAllHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hell getAll")
+	w.Header().Set("Content-Type", "application/json")
+	rows, err := database.Db.Query(fmt.Sprintf("SELECT * from %s", tableName))
+
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+	}
+
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		task := Task{}
+		if err = rows.Scan(&task.Id, &task.Title, &task.Body, &task.CreatedAt, &task.UpdatedAt); err != nil {
+			continue
+		}
+		tasks = append(tasks, task)
+	}
+
+	var buf bytes.Buffer
+	je := json.NewEncoder(&buf)
+
+	if err = je.Encode(&tasks); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, err = fmt.Fprint(w, buf.String())
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 }
 
 func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	var task Task
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -75,10 +111,18 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
 		return
 	}
 
-	id, _ := res.LastInsertId()
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+
 	task.Id = int(id)
 
 	var buf bytes.Buffer
@@ -86,7 +130,7 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err = je.Encode(&task); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err.Error())
+		fmt.Fprint(w, err)
 		return
 	}
 
